6570f1d297f55dddc97abcdbebebf8032eee5466
trieunguyen272 <thitrieu272@gmail.com>
2022-11-12 15:19:41 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParse from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+
+app.use(bodyParse.json({ limit: "30mb", extended: true }));
+app.use(bodyParse.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://admin:admin12345@cluster0.ida8vwy.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 3500;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
